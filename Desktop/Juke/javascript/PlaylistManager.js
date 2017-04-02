becf5c3a13a571eeb6ecd83ff769cdf46330482b
Lisa Ewart <lnemedium@gmail.com>
2017-04-02 13:31:34 -0400

    style changes

---
+++ Desktop/Juke/javascript/PlaylistManager.js
@@ -0,0 +1,37 @@
+/*******************
+    PLAYLIST MANAGEMENT
+********************/
+
+const PlaylistManager = {};
+
+PlaylistManager.tracks = [];
+
+PlaylistManager.currentSong = 0;
+
+PlaylistManager.addTrack = (track = reqParam()) => {
+    PlaylistManager.tracks.push(track);
+//	PlaylistManager.saveData(PlaylistManager.tracks)
+}; // PlaylistManager.addTrack
+
+PlaylistManager.removeById = (id) => {
+    for (let i = 0; i < PlaylistManager.tracks.length; i++) {
+        const track = PlaylistManager.tracks[i];
+        if (track.id === id) {
+            PlaylistManager.tracks.splice(i, 1);
+            break;
+        }
+    }
+}
+
+PlaylistManager.getNextSong = () => {
+    PlaylistManager.currentSong++;
+    const {tracks, currentSong} = PlaylistManager;
+
+    const len = tracks.length;
+    if (currentSong === len) {
+        PlaylistManager.currentSong = 0;
+    }
+
+    return tracks[PlaylistManager.currentSong].id;
+}
+
